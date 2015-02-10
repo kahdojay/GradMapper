3f897df03124d5b1d997e987357e8a56507850fc
Brian Borge <brianborge@gmail.com>
2015-02-09 22:40:00 -0500

    Create graduate seeder.

---
+++ app/helpers/graduate_seeder.rb
@@ -0,0 +1,16 @@
+module GraduateSeeder
+  def self.seed(graduates)
+    graduates.each do |g|
+      p Graduate.create!(
+        name: g.name,
+        email: g.email,
+        github: g.profile[:github],
+        quora: g.profile[:quora],
+        twitter: g.profile[:twitter],
+        facebook: g.profile[:facebook],
+        linked_in: g.profile[:linked_in],
+        dbc_id: g.cohort_id
+      )
+    end
+  end
+end
