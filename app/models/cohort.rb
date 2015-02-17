62295f5df18af2d39c31292f567eafc40963efc0
Don J <kahdojay@gmail.com>
2015-02-17 09:57:55 -0500

    Add soulmate load/delete methods to models to persist data in Retis for autocomplete.

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,16 @@
 class Cohort < ActiveRecord::Base
   has_many :graduates, primary_key: "dbc_id", foreign_key: "dbc_id"
+  after_save :load_into_soulmate
+  before_destroy :remove_from_soulmate
+
+  private
+  def load_into_soulmate
+    loader = Soulmate::Loader.new("cohorts")
+    loader.add("term" => name, "id" => id, "data" => { "link" => Rails.application.routes.url_helpers.cohort_path(self) })
+  end
+
+  def remove_from_soulmate
+    loader = Soulmate::Loader.new("cohorts")
+    loader.remove("id" => id)
+  end
 end
--- app/models/graduate.rb
+++ app/models/graduate.rb
@@ -1,14 +1,26 @@
 class Graduate < ActiveRecord::Base
   belongs_to :cohort, foreign_key: "dbc_id"
+  after_save :load_into_soulmate
+  before_destroy :remove_from_soulmate
   after_create :linkedin_scraper
+
+  private
+
+  def load_into_soulmate
+    loader = Soulmate::Loader.new("graduates")
+    loader.add("term" => name, "id" => id, "data" => { "link" => Rails.application.routes.url_helpers.graduate_path(self) })
+  end
+
+  def remove_from_soulmate
+    loader = Soulmate::Loader.new("graduates")
+    loader.remove("id" => id)
+  end
 
   def linkedin_scraper
     return ("nil") if (linked_in.nil?)
     return ("empty") if (linked_in.empty?)
     get_linkedin(linked_in) if (linked_in.include?("linkedin") || linked_in.include?("lnkd"))
   end
-
-  private
 
   def get_linkedin(link)
     profile = Linkedin::Profile.get_profile(link)
