4666623220f3d75f20ab3505b488d7c1e484aab0
Don J <kahdojay@gmail.com>
2015-02-19 16:36:46 -0500

    Refactor/improve linkedin scrape.

--- app/models/graduate.rb
+++ app/models/graduate.rb
@@ -2,7 +2,7 @@ class Graduate < ActiveRecord::Base
   belongs_to :cohort, foreign_key: "dbc_id"
   after_save :load_into_soulmate
   before_destroy :remove_from_soulmate
-  after_create :linkedin_scraper
+  after_create :scrape_linkedin
 
   private
 
@@ -16,23 +16,25 @@ class Graduate < ActiveRecord::Base
     loader.remove("id" => id)
   end
 
-  def linkedin_scraper
+  def scrape_linkedin
     return ("nil") if (linked_in.nil?)
     return ("empty") if (linked_in.empty?)
-    get_linkedin(linked_in) if (linked_in.include?("linkedin") || linked_in.include?("lnkd"))
+    get_linkedin_profile(linked_in) if (linked_in.include?("linkedin") || linked_in.include?("lnkd"))
   end
 
-  def get_linkedin(link)
+  def get_linkedin_profile(link)
     profile = Linkedin::Profile.get_profile(link)
-
-    if !profile.nil? && !profile.name.nil? && !profile.location.nil? && !profile.current_companies.empty? && !grad_location(profile).empty?
-      coordinates = grad_location(profile)[0].data["geometry"]["location"]
-      update_grad(profile, coordinates)
+    if profile && profile.location && profile.current_companies[0]
+      p "Seeding #{profile.name}'s location and company"
+      update(location: profile.location, company: profile.current_companies[0][:company])
+      search = Geocoder.search("#{profile.location} #{profile.current_companies[0][:company]}")
+      if search[0]
+        p "Seeding #{profile.name}'s coordinates"
+        lat = search[0].latitude
+        lng = search[0].longitude
+        update(lat: lat, long: lng)
+      end
     end
-  end
-
-  def grad_location(profile)
-    Geocoder.search("#{profile.location}, #{profile.current_companies[0][:company]}")
   end
 
   def update_grad(profile, coordinates)
