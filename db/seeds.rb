2454091814e16bbdaa1b06b88978f28c15b9a011
Don J <kahdojay@gmail.com>
2015-02-23 05:03:38 -0500

    Comment out graduate seeding from seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-include CohortSeeder
-include GraduateSeeder
+include DBCSeeder
+# include GraduateSeeder
 
 DBC.token = ENV["DBC_KEY"]
 cohorts = DBC::Cohort.all
-alumn_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
-graduates = alumn_cohorts.collect { |c| c.students }.flatten
+alum_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
+# graduates = alum_cohorts.collect { |c| c.students }.flatten
 
-CohortSeeder::seed(alumn_cohorts)
-GraduateSeeder::seed(graduates)
+DBCSeeder::seed(alum_cohorts)
+# GraduateSeeder::seed(graduates)
+
+# cohorts.each do |cohort|
+#   cohort.students.each do |student|
+#     target_name = student.name
+#     Student.find_by(name: target_name).update(cohort: cohort)
+#   end
+# end
