fc33b29ed653c02127c73c0916947e5ea6ea6c24
Don J <kahdojay@gmail.com>
2015-03-16 15:20:43 -0400

    Refactor map.js (remove unused code, more logical ordering).

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -11,37 +11,44 @@ var initializeMap = function() {
   window.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-// var initializeSoulmate = function() {
-//   var render = function(term, data, type) {
-//     return term;
-//   }
-//   var select = function(term, data, type) {
-//     $("#search").text(term);
-//     $("#search").val(term);
-//     $("ul#soulmate").hide();
-//     clearMarkers();
-//     populateMarkers($(event.target).text())
-//   }
-//   var checkForEmptySearch = function() {
-//     if ($("#search").val() === "") { populateMarkers() }
-//   }
+var populateGraduates = function(graduates) {
+  $('.carousel').slick('unslick');
+  $("#grad-container").empty();
 
-//   $("#search").soulmate({
-//     url: "/autocomplete/search",
-//     types: ["cohorts"],
-//     renderCallback: render,
-//     selectCallback: select,
-//     minQueryLength: 2,
-//     maxResults: 5
-//   })
-//   $(window).keydown(function(event){
-//     if(event.keyCode == 13) {
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-//   $(window).keyup(function(event){ checkForEmptySearch() });
-// }
+  graduates.forEach(function(graduate) {
+    var latitude_longitude = new google.maps.LatLng((parseFloat(graduate.lat)), (parseFloat(graduate.long)))
+    var marker = new google.maps.Marker({
+      position: latitude_longitude,
+      map: window.map
+    })
+    var contentString = "<div class='grad-panel'><img class='grad-pic' src=" + graduate.img_url + "><div class='grad-content'><p class='grad-name'>" + graduate.name + "</p><p class='grad-cohort_name'>" + graduate.cohort_name + "</p><p class='grad-company'>" + graduate.company + "</p>" + "<a href=" + graduate.linked_in + ">LinkedIn </a></div></div>"
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 140
+    })
+    $(".carousel").append(contentString);
+    markersArray.push(marker);
+    infoBoxes.push(infoWindow);
+    google.maps.event.addListener(marker, 'mouseover', function() {
+      infoBoxes.forEach(function(infoBox) {
+        infoBox.close();
+      })
+      infoWindow.open(window.map, marker);
+    });
+  })
+  $(".buttons-container").append('<p id="up">&#9650</p><p id="down">&#9660</p>')
+  initializeSlick();
+}
+
+var populateMarkers = function(cohortName) {
+  $.ajax({
+    type: "GET",
+    url: "/graduates",
+    data: {cohort: cohortName}
+  }).done(function(response) {
+    populateGraduates(response)
+  })
+}
 
 var initializeAutoComplete = function() {
   var fruits = ['Apple', 'Banana', 'Orange'];
@@ -65,69 +72,9 @@ var initializeSlick = function() {
   $('.carousel').find('.grad-panel')
 }
 
-
-var populateMarkers = function(cohortName) {
-  $.ajax({
-    type: "GET",
-    url: "/graduates",
-    data: {cohort: cohortName}
-  }).done(function(response) {
-    populateGraduates(response)
-  })
-}
-
-var populateGraduates = function(graduates) {
-  $('.carousel').slick('unslick');
-  $("#grad-container").empty();
-
-  var indexCounter = 0
-
-  graduates.forEach(function(graduate) {
-    indexCounter = indexCounter + 1
-
-    var latitude_longitude = new google.maps.LatLng((parseFloat(graduate.lat)), (parseFloat(graduate.long)))
-    var marker = new google.maps.Marker({
-      position: latitude_longitude,
-      map: window.map
-    })
-
-    var contentString = "<div class='grad-panel'><img class='grad-pic' src=" + graduate.img_url + "><div class='grad-content'><p class='grad-name'>" + graduate.name + "</p><p class='grad-cohort_name'>" + graduate.cohort_name + "</p><p class='grad-company'>" + graduate.company + "</p>" + "<a href=" + graduate.linked_in + ">LinkedIn </a></div></div>"
-    var infoWindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 140
-    })
-
-      $(".carousel").append(contentString);
-
-
-    markersArray.push(marker);
-    infoBoxes.push(infoWindow);
-    google.maps.event.addListener(marker, 'mouseover', function() {
-      infoBoxes.forEach(function(infoBox) {
-        infoBox.close();
-      })
-      infoWindow.open(window.map, marker);
-    });
-  })
-  $(".buttons-container").append('<p id="up">&#9650</p><p id="down">&#9660</p>')
-  initializeSlick();
-}
-
-var slideToMarkerId = function(index) {
-  $(".carousel").slick('slickGoTo', index);
-  console.log("went to " + index)
-}
-
-var clearMarkers = function() {
-  for (var i=0; i < markersArray.length; i++) {
-    markersArray[i].setMap(null);
-  }
-  markersArray.length = 0;
-}
-
 $(document).ready(function(){
   initializeMap();
-  initializeSlick();
   populateMarkers();
+  initializeSlick();
   initializeAutoComplete();
 })
