394fe2ee27a8168b4e6cc4607bfde85530794c38
Don J <kahdojay@gmail.com>
2015-03-16 17:26:31 -0400

    Remove unused code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,6 @@
 include DBCSeeder
-# include GraduateSeeder
 
 DBC.token = ENV["DBC_KEY"]
 cohorts = DBC::Cohort.all
 alum_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
-# graduates = alum_cohorts.collect { |c| c.students }.flatten
-
 DBCSeeder::seed(alum_cohorts)
-# GraduateSeeder::seed(graduates)
-
-# cohorts.each do |cohort|
-#   cohort.students.each do |student|
-#     target_name = student.name
-#     Student.find_by(name: target_name).update(cohort: cohort)
-#   end
-# end
