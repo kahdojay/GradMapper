78fa52114aebbb7adbedc45a62efbcd1ce068f08
Don J <kahdojay@gmail.com>
2015-03-17 16:06:22 -0400

    Move seed helper method to cohort model, remove unused code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 DBC.token = ENV["DBC_KEY"]
 cohorts = DBC::Cohort.all
 alum_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
-DBCSeeder::seed(alum_cohorts)
+Cohort.seed(alum_cohorts)
