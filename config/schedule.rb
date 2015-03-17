1f10766cd779dd039d3c871e8224939c9f525ccf
Don J <kahdojay@gmail.com>
2015-03-17 14:09:35 -0400

    Remove commented code.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,13 @@
-set :output, "/log/cron_log.log"
+# set :output, "/log/cron_log.log"
 
-every 1.hour do
-  runner "Graduate.scrape_li"
-end
-
-every :reboot do
-  rake "bundle exec rake:db drop"
-  rake "bundle exec rake:db create"
-  rake "bundle exec rake:db migrate"
-  rake "bundle exec rake:db seed"
-  runner "Graduate.scrape_li"
-end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
+# every 1.hour do
+#   runner "Graduate.scrape_li"
 # end
 
-# Learn more: http://github.com/javan/whenever
+# every :reboot do
+#   rake "bundle exec rake:db drop"
+#   rake "bundle exec rake:db create"
+#   rake "bundle exec rake:db migrate"
+#   rake "bundle exec rake:db seed"
+#   runner "Graduate.scrape_li"
+# end
