59cb837e84d392e8b265e2ecd190f274c9624a5d
Don J <kahdojay@gmail.com>
2015-03-22 16:21:00 -0400

    Update db files, no more pushing up lines of canned seed data.

--- db/seeds original.rb
+++
@@ -1,4 +0,0 @@
-DBC.token = ENV["DBC_KEY"]
-cohorts = DBC::Cohort.all
-alum_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
-Cohort.seed(alum_cohorts)
