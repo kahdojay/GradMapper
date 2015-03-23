b3293692e88a00946f1b9ee53ba944f8fb0e25b6
Don J <kahdojay@gmail.com>
2015-03-23 16:40:23 -0400

    Puts cohort name while seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 DBC.token = ENV["DBC_KEY"]
 cohorts = DBC::Cohort.all
 # alum_cohorts = cohorts.select { |c| (c.in_session == false) && (Time.parse(c.start_date) < Time.now) }
+# cohorts.each {|c| p c.name}
 Cohort.seed(cohorts)
